02c58830f7fc0d7ec2f7f49ab70fc9b1d217b8cc
Xevion <xevioni@yandex.com>
2020-10-31 20:37:31 -0500

    create new File model for better Directory and file thumbnail state management

--- viewer/models.py
+++ viewer/models.py
@@ -1,6 +1,13 @@
+import mimetypes
+import os
 import uuid
 
 from django.db import models
+from django.urls import reverse
+from easy_thumbnails.alias import aliases
+
+if not aliases.get('small'):
+    aliases.set('small', {'size': (150, 80), 'crop': True})
 
 
 class ServedDirectory(models.Model):
@@ -20,5 +27,54 @@ class ServedDirectory(models.Model):
     regex = models.BooleanField('Directory RegEx Option', default=False)
     match_filename = models.BooleanField('RegEx Matches Against Filename', default=True)
 
+    def refresh(self):
+        """Refresh the directory listing to see if any new files have appeared and add them to the list."""
+        for file in os.listdir(self.path):
+            # Check if the file has been entered before
+            entry = self.files.filter(filename__exact=file).first()
+            if entry is None:
+                # create the file entry
+                entry = File.create(full_path=os.path.join(self.path, file))
+                entry.save()
+
     def __str__(self):
         return self.path
+
+
+class File(models.Model):
+    path = models.CharField('Full Filepath', max_length=300)
+    filename = models.CharField('Filename', max_length=160)
+    mediatype = models.CharField('Mediatype', max_length=30)
+    directory = models.ForeignKey(ServedDirectory, on_delete=models.CASCADE, related_name='files')
+
+    @classmethod
+    def create(cls, full_path: str) -> 'File':
+        """Simple shortcut for creating a File database entry with just the path."""
+        return File(
+            path=full_path,
+            filename=os.path.basename(full_path),
+            mediatype=File.get_mediatype(full_path),
+            directory=os.path.dirname(full_path)
+        )
+
+    def get_url(self, directory: ServedDirectory) -> str:
+        """Retrieve the direct URL for a given file."""
+        return reverse('file', args=(directory.id, self.filename))
+
+    @staticmethod
+    def get_mediatype(path) -> str:
+        """Simple media type categorization based on the given path."""
+        if os.path.exists(path):
+            if os.path.isdir(path):
+                return 'folder'
+            mimetype = mimetypes.guess_type(path)[0]
+            if mimetype is not None:
+                if mimetype.startswith('image'):
+                    return 'image'
+                elif mimetype.startswith('video'):
+                    return 'video'
+            return 'file'
+        return 'unknown'
+
+    def __str__(self) -> str:
+        return self.filename
